d86addb40329e53cb72cf13602f3b05a04e619c5
mJagielski <mJagielski@ND26257.ordsvy.gov.uk>
2016-07-19 14:10:05 +0100

    db properties

--- counting-centre/src/main/java/com/hackday/VotingRepository.java
+++ counting-centre/src/main/java/com/hackday/VotingRepository.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
-public interface VotingRepository extends Repository{
+public interface VotingRepository extends Repository<Vote, Long>{
 	@Query("select candidateId, count(1) from Vote v group by candidateId")
 	public Map<String, Integer> getVotes();
 	
