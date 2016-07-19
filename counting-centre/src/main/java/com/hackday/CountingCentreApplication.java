2646654d3177a8c61d0cd700724b83ed47d442e8
mJagielski <mJagielski@ND26257.ordsvy.gov.uk>
2016-07-19 11:49:56 +0100

    Add counting-centre service

---
+++ counting-centre/src/main/java/com/hackday/CountingCentreApplication.java
@@ -0,0 +1,12 @@
+package com.hackday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountingCentreApplication.class, args);
+	}
+}
