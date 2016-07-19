905e40ec94c4c07faf05f13bf16cb905da76b811
mJagielski <mJagielski@ND26257.ordsvy.gov.uk>
2016-07-19 14:37:42 +0100

    repository change

--- polling-station/src/main/java/com/hackday/VotesRepository.java
+++ polling-station/src/main/java/com/hackday/VotesRepository.java
@@ -2,7 +2,9 @@ package com.hackday;
 
 import java.util.List;
 
-public interface VotesRepository {
+import org.springframework.data.repository.Repository;
+
+public interface VotesRepository extends Repository<Vote, Long>{
 	public List<Vote> getAll();
 
 	public void save(Vote vote);
