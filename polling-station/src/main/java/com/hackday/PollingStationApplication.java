4ee676c3b9f3b706e4639d68ec6064c7a9c12e28
mJagielski <mJagielski@ND26257.ordsvy.gov.uk>
2016-07-19 11:43:10 +0100

    Add polling station service

---
+++ polling-station/src/main/java/com/hackday/PollingStationApplication.java
@@ -0,0 +1,12 @@
+package com.hackday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingStationApplication.class, args);
+	}
+}
